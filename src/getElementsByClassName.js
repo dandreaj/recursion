b3a5a52fd4e6ce35832500ce1a3644447415038a
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-05-07 17:17:31 -0500

    Completed bare minimum assignments, JSON.stringify and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var allElements = document.body;
+  var elements = [];
+  
+  function scanElements(element) {
+    if(element.classList !== undefined && element.classList.contains(className)){
+      elements.push(element);
+    }
+    for(var i = 0;i < element.childNodes.length; i++) {
+      scanElements(element.childNodes[i]);
+    }
+  }
+
+  scanElements(allElements);
+  return elements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var type = typeof obj;
+
+  //If obj is null should return 'null'
+  if (obj === null) {
+    return "null";
+  }
+
+  //if obj is not defined or is a function, cannot stringify
+  if (obj === undefined || type === "function") {
+    return;
+  }
+
+  //Strings: if the obj is a string, return obj
+  if (type === "string") {
+    return '"' + obj + '"';
+  }
+
+  //Numbers & Booleans: Change value to string and return
+  if (type === "number" || type === "boolean") {
+    return obj.toString();
+  }
+
+  //Arrays: User for loop to iterate through array to stringify each element and join together
+  if (Array.isArray(obj)) {
+    var arrayString = "["
+
+    for (var i = 0; i < obj.length; i++) {
+      arrayString = arrayString + stringifyJSON(obj[i]) + (i < obj.length - 1 ? "," : "");
+    }
+    return arrayString + "]";
+  }
+
+  //Objects: Iterate through to stringify key value pairs
+  if (type === "object") {
+    var keys = Object.keys(obj);
+    var objectString = "{";
+    var keyCounter = 0;
+
+    for (var key in obj) {
+      //value of key must be defined
+      if (stringifyJSON(obj[key])) {
+        objectString = objectString + stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + (keyCounter < keys.length - 1 ? "," : "");
+        keyCounter++;
+      }
+    }
+    return objectString + "}";
+  }
+
 };
